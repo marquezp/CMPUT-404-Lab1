26c43e58294b73bbc2b9f916d6f8c9c02dd4c2a8
marquezp <marquezp@ualberta.ca>
2023-01-10 10:58:42 -0700

    update

--- lab1_script.py
+++ lab1_script.py
@@ -2,4 +2,7 @@ import requests
 
 print(requests.__version__)
 result = requests.get("http://www.google.com")
-print(result.status_code)
+# print(result.status_code)
+
+raw = requests.get("https://raw.githubusercontent.com/marquezp/CMPUT-404-Lab1/main/lab1_script.py")
+print(raw.text)
